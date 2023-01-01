78f36c71b16a699d9197451efb72165008fde2f7
Fernando <ferobregon03@gmail.com>
2023-01-01 22:49:52 +0100

    added parallax effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Parallax, ParallaxLayer } from 'svelte-parallax';
+</script>
+
+<div class="sticky top-0 bg-red-300 w-screen h-[10vh] z-20" />
+<div class="bg-gray-200">
+  <Parallax sections={3}>
+    <ParallaxLayer offset={0.25} span={2} rate={0.3}>
+      <h1 class="text-5xl text-center">
+        Hey, I'm
+        <br />
+        Fernando Obregon
+      </h1>
+    </ParallaxLayer>
+    <ParallaxLayer rate={-2}>
+      <b
+        class="text-[400px] text-center opacity-30 absolute top-[-100px] left-[50%] translate-x-[-50%] "
+        >OFF</b
+      ></ParallaxLayer
+    >
+    <ParallaxLayer offset={0.75}>
+      <div
+        class="w-[80vw] h-full mx-auto  border-t-2 border-gray-500 bg-blue-100"
+      >
+        I'm changing the background color!
+      </div>
+    </ParallaxLayer>
+
+    <ParallaxLayer
+      offset={1}
+      rate={-2.5}
+      style="display: flex; justify-content: flex-end;"
+    >
+      <div
+        style="background-color: lightblue; opacity: 0.5; width: 50%; height: 100%;"
+      />
+    </ParallaxLayer>
+
+    <ParallaxLayer
+      offset={1}
+      rate={2.5}
+      style="display: flex; justify-content: flex-start;"
+    >
+      <div
+        style="background-color: yellow; opacity: 0.5; width: 50%; height: 100%;"
+      />
+    </ParallaxLayer>
+  </Parallax>
+</div>
