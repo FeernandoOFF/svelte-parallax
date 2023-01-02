f205db000b9ed3ffdc899b434e7e28dc48d5e66c
Fernando <ferobregon03@gmail.com>
2023-01-02 10:12:00 +0100

    Updated readme & Created other effects

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 - Bun
 - Tailwind
+- Svelte
+
+## Learning
+
+This test helped me to understand and the **foundation** to create better parallax effects
+
+Core concepts:
+
+- Parallax Container
+  A block which position is relative and will be the containing block for all the movement, this receives a parameter named **sections** which basically is the ammount of screens
+- Parallax Layers
+  Parallax layers are the actual absolute layers that will move across the container at **different speed** and will appear in different
+
+- Sticky Layer
+  Same as a Parallax layer, with the different that this will stick from a screen to another one
+
+### Improvements:
+
+- Make big changes based on smaller but keep the proportions. ie - change from 0.5 to 0.6 and make it smaller/bigger in that proportion (probably `onProgress`)
+
+- Change `layout` of `sticky` based on the progress
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-  import { Parallax, ParallaxLayer } from 'svelte-parallax';
+  import { Parallax, ParallaxLayer, StickyLayer } from 'svelte-parallax';
 </script>
 
 <div class="sticky top-0 bg-red-300 w-screen h-[10vh] z-20" />
 <div class="bg-gray-200">
-  <Parallax sections={3}>
+  <Parallax sections={4}>
+    <!-- Titles -->
     <ParallaxLayer offset={0.25} span={2} rate={0.3}>
       <h1 class="text-5xl text-center">
         Hey, I'm
@@ -12,12 +13,16 @@
         Fernando Obregon
       </h1>
     </ParallaxLayer>
-    <ParallaxLayer rate={-2}>
-      <b
-        class="text-[400px] text-center opacity-30 absolute top-[-100px] left-[50%] translate-x-[-50%] "
-        >OFF</b
-      ></ParallaxLayer
-    >
+    <ParallaxLayer rate={2} let:progress>
+      <div class="mx-auto w-full text-center">
+        <b
+          class="text-[400px] text-center opacity-30 "
+          style="transform: scale({(progress > 0.2 ? progress : 0.5) * 100}%);"
+          >OFF
+        </b>
+      </div>
+    </ParallaxLayer>
+    <!-- Text line -->
     <ParallaxLayer offset={0.75}>
       <div
         class="w-[80vw] h-full mx-auto  border-t-2 border-gray-500 bg-blue-100"
@@ -26,24 +31,44 @@
       </div>
     </ParallaxLayer>
 
+    <!-- Yellow layer -->
+    <StickyLayer offset={{ top: 1.2, bottom: 2 }} let:progress>
+      <div class="flex relative h-full transition-all">
+        <div
+          style="background-color: yellow; opacity: 0.5; width: 50%; height: 100%;"
+          class="transition-all  absolute {progress > 0.5
+            ? 'right-0'
+            : 'left-0'} grid place-items-center"
+        >
+          <p
+            style="transform:scale({(progress > 0.6 ? progress : 0.6) * 200}%)"
+          >
+            I'm getting bigger
+          </p>
+          <h2>{progress}</h2>
+        </div>
+      </div>
+    </StickyLayer>
+    <!-- Half layers -->
     <ParallaxLayer
       offset={1}
-      rate={-2.5}
+      rate={2.5}
       style="display: flex; justify-content: flex-end;"
     >
       <div
         style="background-color: lightblue; opacity: 0.5; width: 50%; height: 100%;"
       />
     </ParallaxLayer>
-
+    <!-- Green layer -->
     <ParallaxLayer
-      offset={1}
+      offset={2}
       rate={2.5}
       style="display: flex; justify-content: flex-start;"
     >
-      <div
-        style="background-color: yellow; opacity: 0.5; width: 50%; height: 100%;"
-      />
+      <div class="w-1/2 bg-green-600 h-full" />
+    </ParallaxLayer>
+    <ParallaxLayer offset={3.5} rate={2.5}>
+      <h2 class="text-[200px] text-center opacity-30 ">That's it!</h2>
     </ParallaxLayer>
   </Parallax>
 </div>
